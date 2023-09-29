008ec75435a504e8f368b8b5aa007884bb9db085
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 09:43:51 -0300

    arreglado dbconfig

--- Servidor/dbconfig.js
+++ Servidor/dbconfig.js
@@ -1,16 +1,13 @@
-import dotenv from 'dotenv';
+import 'dotenv/config'
+  const config = {
 
-dotenv.config();
-
-const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST, 
-    database: process.env.DB_NAME,
-    options: {
-        trustServerCertificate: true,
-        trustedConnection: true,
-    },
-};
-
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database:process.env.DB_DATABASE,
+    options:{
+        trustServerCertificate:true,
+        trustedConnection:true
+    }
+}
 export default config;
